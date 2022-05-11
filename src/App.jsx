23863a5c18d57d55181ae939daad391b7b9f9399
Lastand Development <99511621+LastandDevelopment@users.noreply.github.com>
2022-05-10 21:48:08 -0400

    Fix

--- src/App.jsx
+++ src/App.jsx
@@ -13,8 +13,8 @@ import MeteorExtinctionHome from "./components/MeteorExtinctionHome";
 import MeteorExtinctionDownload from "./components/MeteorExtinctionDownload";
 import WoodenGUIHome from "./components/WoodenGUIHome";
 import WoodenGUIDownload from "./components/WoodenGUIDownload";
-import Contact from "./components/Contact";
-import IssueTemplate from "./components/IssueTemplate";
+import Contact from "./components/ContactPage";
+import IssueTemplate from "./components/ReportIssue";
 import CheckMEVersion from "./components/CheckMEVersion";
 
 import CustomProgress from "./components/SuspenseFallback";
